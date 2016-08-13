b8a3288093f1d3eec6ebd6c991ef89bb4ba88315
Carlos Cardona <cgcardona@gmail.com>
2016-08-13 10:35:01 -0700

    Build out homepage.

--- app/homepage/homepage.js
+++ app/homepage/homepage.js
@@ -9,13 +9,15 @@ angular.module('blockchain.homepage', ['ngRoute'])
   });
 }])
 
-.controller('HomepageCtrl', ['$scope', '$http', function($scope, $http) {
+.controller('HomepageCtrl', ['$scope', '$http', '$log', function($scope, $http, $log) {
+  $scope.$log = $log;
+  var homepageController = this;
+  homepageController.blocks = [];
   $http({
     method: 'GET',
     url: 'http://btc.blockr.io/api/v1/coin/info'
   }).then(function successCallback(response) {
     var data = response.data.data;
-    console.log(data);
     $scope.value = data.markets.coinbase.value;
     $scope.all = data.volume.all;
     $scope.current = data.volume.current;
@@ -25,6 +27,27 @@ angular.module('blockchain.homepage', ['ngRoute'])
     $scope.next_difficulty = data.next_difficulty.difficulty;
     $scope.next_difficulty_perc = data.next_difficulty.perc;
     $scope.retarget_in = data.next_difficulty.retarget_in;
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+  
+  $http({
+    method: 'GET',
+    url: 'http://btc.blockr.io/api/v1/block/info/416614,416613,416612,416611,416610,416609,416608,416607,416606,416605,416604,416603,416602,416601,416600,416599,416598,416597,416596,416595'
+  }).then(function successCallback(response) {
+    var data = response.data.data;
+    angular.forEach(data, function(block) {
+      homepageController.blocks.push({
+        nb: block.nb,
+        time_utc: block.time_utc,
+        nb_txs: block.nb_txs,
+        fee: block.fee,
+        size: block.size,
+        days_destroyed: block.days_destroyed
+        
+      });
+    });
   }, function errorCallback(response) {
     // called asynchronously if an error occurs
     // or server returns response with an error status.
