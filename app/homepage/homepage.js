a632a361d4856efc97b2206c030c575a6178e912
Carlos Cardona <cgcardona@gmail.com>
2016-08-13 10:12:02 -0700

    Add homepage hero.

--- app/homepage/homepage.js
+++ app/homepage/homepage.js
@@ -9,6 +9,25 @@ angular.module('blockchain.homepage', ['ngRoute'])
   });
 }])
 
-.controller('HomepageCtrl', [function() {
+.controller('HomepageCtrl', ['$scope', '$http', function($scope, $http) {
+  $http({
+    method: 'GET',
+    url: 'http://btc.blockr.io/api/v1/coin/info'
+  }).then(function successCallback(response) {
+    var data = response.data.data;
+    console.log(data);
+    $scope.value = data.markets.coinbase.value;
+    $scope.all = data.volume.all;
+    $scope.current = data.volume.current;
+    $scope.perc = data.volume.perc;
+    $scope.market_cap = data.markets.coinbase.value * data.volume.current;
+    $scope.difficulty = data.last_block.difficulty;
+    $scope.next_difficulty = data.next_difficulty.difficulty;
+    $scope.next_difficulty_perc = data.next_difficulty.perc;
+    $scope.retarget_in = data.next_difficulty.retarget_in;
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
 
 }]);
