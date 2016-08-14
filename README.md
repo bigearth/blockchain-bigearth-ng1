e118a5fbf405cb6178ba5d411d7dc690390e89d0
Carlos Cardona <cgcardona@gmail.com>
2016-08-14 09:55:12 -0700

    Format homepage.

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Built w/
 1. `git clone git@github.com:bigearth/blockchain-bigearth-ng1.git && cd blockchain-bigearth-ng1`
 2. `npm install`
 3. `npm start`
-
+  
 ## License 
 
 It's available under the [MIT License](https://opensource.org/licenses/MIT).
--- app/homepage/homepage.js
+++ app/homepage/homepage.js
@@ -26,25 +26,32 @@ angular.module('blockchain.homepage', ['ngRoute'])
     $scope.next_difficulty = data.next_difficulty.difficulty;
     $scope.next_difficulty_perc = data.next_difficulty.perc;
     $scope.retarget_in = data.next_difficulty.retarget_in;
-  }, function errorCallback(response) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-  });
-  
-  $http({
-    method: 'GET',
-    url: 'https://btc.blockr.io/api/v1/block/info/416614,416613,416612,416611,416610,416609,416608,416607,416606,416605,416604,416603,416602,416601,416600,416599,416598,416597,416596,416595'
-  }).then(function successCallback(response) {
-    var data = response.data.data;
-    angular.forEach(data, function(block) {
-      homepageController.blocks.push({
-        nb: block.nb,
-        time_utc: block.time_utc,
-        nb_txs: block.nb_txs,
-        fee: block.fee,
-        size: block.size,
-        days_destroyed: block.days_destroyed
+    
+    var blockHeights = [];
+    var lastBlockHeight = data.last_block.nb;
+    var lowerblockHeight = lastBlockHeight - 19;
+    for(var i = lastBlockHeight; i >= lowerblockHeight; i--){
+      blockHeights.push(i);
+    }
+    
+    $http({
+      method: 'GET',
+      url: 'https://btc.blockr.io/api/v1/block/info/' + blockHeights.join(',')
+    }).then(function successCallback(response) {
+      var data = response.data.data;
+      angular.forEach(data, function(block) {
+        homepageController.blocks.push({
+          nb: block.nb,
+          time_utc: block.time_utc,
+          nb_txs: block.nb_txs,
+          fee: block.fee,
+          size: block.size,
+          days_destroyed: block.days_destroyed
+        });
       });
+    }, function errorCallback(response) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
     });
   }, function errorCallback(response) {
     // called asynchronously if an error occurs
